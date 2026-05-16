51256e22126a20b3e4ef8d95ca21f5da6f6637c3
RegorForgot <regor.sableye@gmail.com>
2026-05-16 15:19:51 +1000

    cleanup and update helm CRC code

--- helm/src/sensors.c
+++ helm/src/sensors.c
@@ -5,9 +5,7 @@
  */
 
 #include "battery.h"
-
 #include "common.h"
-
 #include "gatt.h"
 #include "imu.h"
 #include "magnet.h"
@@ -15,10 +13,9 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/crc.h>
 
-#define STACK_SIZE        2048
-#define PRIORITY          3
-#define THREAD_DELAY      (5 * 1000)
-#define BAT_PACKET_PERIOD (9 * 1000)
+#define BAT_PACKET_PERIOD_MS 9000
+#define PRIORITY             3
+#define STACK_SIZE           2048
 
 #ifdef CHIP_A
 #define NODE_NUM 0
@@ -44,6 +41,7 @@ static void send_sensor_thread(void *arg1, void *arg2, void *arg3)
     ARG_UNUSED(arg1);
     ARG_UNUSED(arg2);
     ARG_UNUSED(arg3);
+
     int64_t magnet_time_ms = 0;
     int64_t battery_time_ms = 0;
 
@@ -54,26 +52,24 @@ static void send_sensor_thread(void *arg1, void *arg2, void *arg3)
         k_msgq_get(&magnet_time_q, &magnet_time_ms, K_NO_WAIT);
         int64_t current = k_uptime_get();
 
+        // Collect Magnet & IMU Data
         sensor_packet.data.sensor.magnet_dt = (current - magnet_time_ms);
-        // Collect Accelerometer & Gyro Data
         k_msgq_get(&imu_q, &(sensor_packet.data.sensor.imu_data), K_NO_WAIT);
 
-        sensor_packet.crc16 =
-            crc16_ansi((char *)&(sensor_packet.data.sensor), sizeof(struct sensor_packet));
+        sensor_packet.crc16 = crc16_ansi((char *)&(sensor_packet.data), sizeof(union ble_data));
 
         // Send to Base
-        send_data_nus(&sensor_packet, sizeof(struct ble_packet));
+        send_data_nus(&sensor_packet, sizeof(sensor_packet));
 
-        // Send Battery Data every BAT_PACKET_PERIOD
-        if ((current - battery_time_ms) < BAT_PACKET_PERIOD) {
+        // Send Battery Data every BAT_PACKET_PERIOD_MS
+        if ((current - battery_time_ms) < BAT_PACKET_PERIOD_MS) {
             continue;
         }
 
         battery_time_ms = current;
         if (get_battery_charge(&(bat_packet.data.bat.bat_charge_pc)) == 0) {
-            bat_packet.crc16 =
-                crc16_ansi((char *)&(sensor_packet.data.bat), sizeof(struct bat_packet));
-            send_data_nus(&bat_packet, sizeof(struct ble_packet));
+            bat_packet.crc16 = crc16_ansi((char *)&(sensor_packet.data), sizeof(union ble_data));
+            send_data_nus(&bat_packet, sizeof(bat_packet));
         }
     }
 }
