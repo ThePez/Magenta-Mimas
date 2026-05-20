f6c2f3c4b0cc215f4efc81a08a4cef429fbc3735
Eden Mehr <e.mehr@student.uq.edu.au>
2026-05-20 13:25:01 +1000

    testing

--- base/src/ukf.h
+++ base/src/ukf.h
@@ -11,7 +11,7 @@
 #define NUM_MEAS     2
 #define SIGMA_POINTS (2 * NUM_STATES + 1)
 
-#define PULSE_DELAY 250
+#define PULSE_DELAY 100
 #define RADIUS  0.125
 #define Q_OMEGA 0.1 /* higher value more unreliable system*/
 #define BIAS_A  1e-3
