e4541d3d38ed4216733814bad1396b3659aa5b29
Jack <s4358260@student.uq.edu.au>
2026-05-20 16:43:33 +1000

    new json stuff for incoming commands

--- base/src/json.c
+++ base/src/json.c
@@ -25,28 +25,38 @@ static const struct json_obj_descr json_descr[] = {
     JSON_OBJ_DESCR_PRIM(struct json_packet, direction, JSON_TOK_INT),
 };
 
+static const struct json_obj_descr cmd_descr[] = {
+    JSON_OBJ_DESCR_PRIM(struct cmd_packet, cmd, JSON_TOK_UINT),
+    JSON_OBJ_DESCR_PRIM(struct cmd_packet, pulse, JSON_TOK_UINT),
+    JSON_OBJ_DESCR_PRIM(struct cmd_packet, time, JSON_TOK_INT64),
+};
+
 /* ========================================================================== */
 /* Encode / Decode                                                            */
 /* ========================================================================== */
+
+int encode_cmd_packet(struct cmd_packet *val, char *buffer, size_t buf_size)
+{
+    int ret = json_obj_encode_buf(cmd_descr, ARRAY_SIZE(cmd_descr), val, buffer, buf_size);
+    return (ret);
+}
+
+int decode_cmd_packet(char *input, size_t len, struct cmd_packet *data)
+{
+    int ret = json_obj_parse(input, len, cmd_descr, ARRAY_SIZE(cmd_descr), data);
+    return (ret);
+}
 
 /* Encodes a cmd_json struct into a JSON string in the provided buffer. */
 int encode_json_packet(struct json_packet *val, char *buffer, size_t buf_size)
 {
     int ret = json_obj_encode_buf(json_descr, ARRAY_SIZE(json_descr), val, buffer, buf_size);
-    if (ret < 0) {
-        return (ret);
-    }
-
-    return (0);
+    return (ret);
 }
 
 /* Decodes a JSON string into a cmd_json struct. */
 int decode_json_packet(char *input, size_t len, struct json_packet *data)
 {
     int ret = json_obj_parse(input, len, json_descr, ARRAY_SIZE(json_descr), data);
-    if (ret < 0) {
-        return (ret);
-    }
-
-    return (0);
+    return (ret);
 }
--- base/src/json.h
+++ base/src/json.h
@@ -9,6 +9,7 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <time.h>
 
 struct helm_packet {
     uint64_t magnet_dt;
@@ -24,6 +25,14 @@ struct json_packet {
     int8_t direction;
 };
 
+struct cmd_packet {
+    uint8_t cmd;
+    uint16_t pulse;
+    time_t time;
+};
+
+int encode_cmd_packet(struct cmd_packet *val, char *buffer, size_t buf_size);
+int decode_cmd_packet(char *input, size_t len, struct cmd_packet *data);
 int encode_json_packet(struct json_packet *val, char *buffer, size_t buf_size);
 int decode_json_packet(char *input, size_t len, struct json_packet *data);
 
