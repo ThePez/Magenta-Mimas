4b1622a3a2015a04015ce0cee1daff1064be3ba9
Jack <s4358260@student.uq.edu.au>
2026-05-23 13:40:41 +1000

    MAC updates

--- include/mac.h
+++ include/mac.h
@@ -12,8 +12,8 @@
 #define NUM_CONNECTIONS 2
 
 const bt_addr_le_t base_addr = {
-    .type = BT_ADDR_LE_RANDOM, .a.val = {0xBB, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF}
-    // FF:EE:DD:CC:BB:BB  <-- replace with Base address
+    .type = BT_ADDR_LE_RANDOM, .a.val = {0x88, 0xA3, 0xC1, 0xA0, 0xF2, 0xC3}
+    // C3:F2:A0:C1:A3:88
 };
 
 /* Helm chip addresses - hardcoded for filtering */
@@ -21,12 +21,12 @@ const bt_addr_le_t helm_addr[NUM_CONNECTIONS] = {
     [0] =
         {
             .type = BT_ADDR_LE_RANDOM, .a.val = {0x56, 0x63, 0xCD, 0x44, 0x4A, 0xE1}
-            // E1:4A:44:CD:63:56 <-- replace with Helm_A address
+            // E1:4A:44:CD:63:56
         },
     [1] =
         {
-            .type = BT_ADDR_LE_RANDOM, .a.val = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF}
-            // FF:EE:DD:CC:BB:AA  <-- replace with Helm_B address
+            .type = BT_ADDR_LE_RANDOM, .a.val = {0xDF, 0x01, 0xF1, 0x02, 0x44, 0xE7}
+            // E7:44:02:F1:01:DF
         },
 };
 
