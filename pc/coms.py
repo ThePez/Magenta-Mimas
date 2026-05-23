92d63838a1b9101f9018c4c63d9647de3effb7f1
RegorForgot <regor.sableye@gmail.com>
2026-05-23 14:00:51 +1000

    Python interim

--- pc/coms.py
+++ pc/coms.py
@@ -388,6 +388,77 @@ class SerialReader(QThread):
         self._running = True
         self._ser = serial_port
 
+    def dashboard_data(data: dict) -> None:
+        timestamp = data["timestamp"]
+
+        node_a = data["nodeA"]
+        node_a_bat = node_a["bat"]
+        node_a_timestamp = node_a_bat["timestamp"]
+
+        node_b = data["nodeB"]
+        node_b_bat = node_b["bat"]
+        node_b_timestamp = node_b_bat["timestamp"]
+
+        velocity = data["speed"] * data["direction"]
+
+        THRESHOLD = 1767225600
+
+        records = []
+
+        if timestamp < THRESHOLD:
+            node_a_conn_p = (
+                Point("nodes")
+                .tag("location", "node_a")
+                .field("conn", node_a["connection_status"])
+                .time(timestamp)
+            )
+
+            node_b_conn_p = (
+                Point("nodes")
+                .tag("location", "node_b")
+                .field("conn", node_b["connection_status"])
+                .time(timestamp)
+            )
+
+            helm_p = (
+                Point("helm")
+                .tag("location", "helm")
+                .field("velocity", velocity)
+                .time(timestamp)
+            )
+
+            records.extend([node_a_conn_p, node_b_conn_p, helm_p])
+
+        if node_a_timestamp < THRESHOLD:
+            node_a_bat_p = (
+                Point("nodes")
+                .tag("location", "node_a")
+                .field("mv", node_a_bat["bat_mv"])
+                .field("chg", node_a_bat["bat_charge"])
+                .time(node_a_timestamp)
+            )
+
+            records.append(node_a_bat_p)
+
+        if node_b_timestamp < THRESHOLD:
+            node_b_bat_p = (
+                Point("nodes")
+                .tag("location", "node_b")
+                .field("mv", node_b_bat["bat_mv"])
+                .field("chg", node_b_bat["bat_charge"])
+                .time(node_b_timestamp)
+            )
+
+            records.append(node_b_bat_p)
+
+        if len(records) > 0:
+            write_api.write(
+                bucket=influx_bucket,
+                org=influx_org,
+                record=records,
+                write_precision=WritePrecision.S,
+            )
+
     def run(self) -> None:
         """
         Main thread loop.
@@ -405,39 +476,7 @@ class SerialReader(QThread):
                         data = json.loads(line)
                         self.log_signal.emit(str(data))
                         if isinstance(data, dict) and self._send_to_server:
-                            # node_a = data["nodeA"]
-                            # node_b = data["nodeB"]
-                            # velocity = data["speed"] * data["direction"]
-
-                            # node_a_p = (
-                            #     Point("nodes")
-                            #     .tag("location", "node_a")
-                            #     .field("conn", node_a["connection_status"])
-                            #     .field("chg", node_a["charge"])
-                            #     .field("mv", node_a["mv"])
-                            # )
-                            # node_b_p = (
-                            #     Point("nodes")
-                            #     .tag("location", "node_b")
-                            #     .field("conn", node_b["connection_status"])
-                            #     .field("chg", node_b["charge"])
-                            #     .field("mv", node_b["mv"])
-                            # )
-                            # helm_p = (
-                            #     Point("helm")
-                            #     .tag("location", "helm")
-                            #     .field("velocity", velocity)
-                            # )
-                            # records = [node_a_p, node_b_p, helm_p]
-
-                            # write_api.write(
-                            #     bucket=influx_bucket,
-                            #     org=influx_org,
-                            #     record=records,
-                            #     write_precision=WritePrecision.S,
-                            # )
-                            pass
-
+                            dashboard_data(data)
                     except json.JSONDecodeError:
                         # Non-JSON output from firmware; pass to logger.
                         self.log_signal.emit(line)
