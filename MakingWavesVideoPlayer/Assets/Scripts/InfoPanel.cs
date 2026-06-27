36a8abc062152f5b5ca5cf94ce8556fca4942735
RegorForgot <regor.sableye@gmail.com>
2026-06-27 23:35:39 +1000

    Add functionality for the F1 button press - all that is needed now is to tie it to the serial

---
+++ MakingWavesVideoPlayer/Assets/Scripts/InfoPanel.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InfoPanel : MonoBehaviour
+{
+    private bool CurrentlyVisible { get; set; }
+    private GameObject infoPanel;
+
+    void Start()
+    {
+        infoPanel = transform.Find("Panel").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current != null && Keyboard.current.f1Key.wasPressedThisFrame)
+        {
+            CurrentlyVisible = !CurrentlyVisible;
+            infoPanel.SetActive(CurrentlyVisible);
+        }
+    }
+}
